2d4ddefb20a79bbf98d3262363a716c7dad7857d
Benjamin <ferrick.tibec@gmail.com>
2019-11-26 23:54:49 +0100

    Update nuget reference from Package.config to PackageReference

--- AvalonAeroAccent/MetroTheme.cs
+++ AvalonAeroAccent/MetroTheme.cs
@@ -15,9 +15,6 @@
   ***********************************************************************************/
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using Xceed.Wpf.AvalonDock.Themes;
 
 namespace AvalonMetroAccent
--- UltimateStreamMgr/ViewModel/LayoutUpdateStrategy.cs
+++ UltimateStreamMgr/ViewModel/LayoutUpdateStrategy.cs
@@ -1,14 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Collections.Specialized;
-using System.ComponentModel;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
+﻿using System.Linq;
 using Xceed.Wpf.AvalonDock.Layout;
 
 namespace UltimateStreamMgr.ViewModel
